e938554b6c0ec4de0bda73d3f7c78954b1e73fa2
Ko-Gyeongtae <kokt0203@naver.com>
2021-01-02 21:22:56 +0900

    add Class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-interface Human{
-    name:string,
-    age:number,
-    gender:string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
 }
 
-const person = {
-    name: "Nicolas",
-    age: 22,
-    gender: "male"
-};
+const Lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 //name, age, gender? => name, age 필수 / gender + ? 선택 argument
-console.log(sayHi(person));
+console.log(sayHi(Lynn));
 export {};
