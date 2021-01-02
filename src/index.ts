6e4c6a897030716a74b5597365cae450d076e89b
Ko-Gyeongtae <kokt0203@naver.com>
2021-01-02 21:47:48 +0900

    Block chain system & Hashing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from "crypto-js";
 class Block {
     public index:number;
     public hash:string;
     public previousHash:string;
     public data:string;
     public timestamp:number;
+
+    static calculateBockHash = (
+        index:number,
+        hash:string, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+    ):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index:number, 
         hash:string, 
@@ -21,6 +31,9 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "299292929292929", "", "Hello", 123456);
 
-let blockChain:[Block] = [genesisBlock];
-console.log(blockChain);
+let blockChain:Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockChain;
+const getLatestBlock = () : Block => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () : number => Math.round[new Date().getTime() / 1000];
 export {};
